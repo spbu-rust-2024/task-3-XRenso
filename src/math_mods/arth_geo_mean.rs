2004c6545b6f2f661afff30984168157ff1a1791
XRenso <tch.xr@ya.ru>
2024-10-26 09:46:02 +0300

    Style: now printing all array of AGM amd MAGM

--- src/math_mods/arth_geo_mean.rs
+++ src/math_mods/arth_geo_mean.rs
@@ -23,16 +23,23 @@ pub fn count() -> i8 {
     let mut a0 = a;
     let mut b0 = b;
 
+    let mut a_vec: Vec<f64> = Vec::new();
+    let mut b_vec: Vec<f64> = Vec::new();
+
     for _ in 0..deep {
+        a_vec.push(a0);
+        b_vec.push(b0);
         a = (a0 + b0) / 2.0;
         b = f64::powf(a0 * b0, 1.0 / 2.0);
         a0 = a;
         b0 = b;
     }
+    a_vec.push(a0);
+    b_vec.push(b0);
 
     println!(
-        "Ваш ответ\n\nАрефметическое: {}\nГеометрическое: {}",
-        a0, b0
+        "Ваш ответ\n\nАрефметическое: {:?}\nГеометрическое: {:?}",
+        a_vec, b_vec
     );
     return math_mods::exit_code_algos();
 }
--- src/math_mods/modif_arth_geo_mean.rs
+++ src/math_mods/modif_arth_geo_mean.rs
@@ -24,17 +24,25 @@ pub fn count() -> i8 {
     let mut y0 = y;
     let mut z0: f64 = 0.0;
     // let mut z = z0;
+    let mut x_vec: Vec<f64> = Vec::new();
+    let mut y_vec: Vec<f64> = Vec::new();
+
     for _ in 0..deep {
+        x_vec.push(x0);
+        y_vec.push(y0);
+
         x = (x0 + y0) / 2.0;
         y = z0 + f64::powf((x0 - z0) * (y0 - z0), 1.0 / 2.0);
         z0 = z0 - f64::powf((x0 - z0) * (y0 - z0), 1.0 / 2.0);
         x0 = x;
         y0 = y;
     }
+    x_vec.push(x0);
+    y_vec.push(y0);
 
     println!(
-        "Ваш ответ\n\nАрефметическое: {}\nГеометрическое: {}",
-        x0, y0
+        "Ваш ответ\n\nАрефметическое: {:?}\nГеометрическое: {:?}",
+        x_vec, y_vec
     );
     return math_mods::exit_code_algos();
 }
